732e4a39cf320da6fa337323c8fb24a5c89d8ff0
lalitakella <31657061+lalitakella@users.noreply.github.com>
2018-12-06 13:42:56 -0500

    ShowMap

--- Assets/__Scripts/TileCamera.cs
+++ Assets/__Scripts/TileCamera.cs
@@ -24,14 +24,14 @@ public class TileCamera : MonoBehaviour
         SPRITES = Resources.LoadAll<Sprite>(mapTiles.name);
         string[] lines = mapData.text.Split('\n'); 
         H = lines.Length;
-        string[] tileNums = lines[0].Split('');
+        string[] tileNums = lines[0].Split(' ');
         W = tileNums.Length;
         System.Globalization.NumberStyles hexNum; 
         hexNum = System.Globalization.NumberStyles.HexNumber;
         MAP = new int[W, H];
         for (int j = 0; j < H; j++)
         {
-            tileNums = lines[j].Split('');
+            tileNums = lines[j].Split(' ');
             for (int i = 0; i < W; i++)
             {
                 if (tileNums[i] == "..")
@@ -46,6 +46,26 @@ public class TileCamera : MonoBehaviour
         }
         print("Parsed " + SPRITES.Length + "sprites.");
         print("Map size: " + W + "wide by " + H + "high");
+
+        ShowMap();
+    }
+    void ShowMap()
+    {
+        TILES = new Tile[W, H];
+        for (int j = 0; j < H; j++)
+        {
+            for (int i = 0; i < W; i++)
+            {
+                if (MAP[i, j] != 0)
+                {
+                    Tile ti = Instantiate<Tile>(tilePrefab); 
+                    ti.transform.SetParent(TILE_ANCHOR);
+                    ti.SetTile(i, j); 
+                    TILES[i, j] = ti;
+                }
+
+            }
+        }
     }
     static public int GET_MAP(int x, int y)
     {
