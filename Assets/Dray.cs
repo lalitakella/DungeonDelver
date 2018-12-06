a63d1029aa85396b64fbabf43f1833b590acd3d5
lalitakella <31657061+lalitakella@users.noreply.github.com>
2018-12-06 14:39:42 -0500

    attack implemented but animations dont work anymore

--- Assets/Dray.cs
+++ Assets/Dray.cs
@@ -4,36 +4,78 @@ using UnityEngine;
 
 public class Dray : MonoBehaviour
 {
+    public enum eMode { idle, move, attack, transition }
+
     [Header("Set in Inspector")]
     public float speed = 5;
+    public float attackDuration = 0.25f;
+    public float attackDelay = 0.5f;
+
     [Header("Set Dynamically")]
-    public int dirHeld = -1; 
+    public int dirHeld = -1;
+    public int facing = 1;
+    public eMode mode = eMode.idle;
+
+    private float timeAtkDone = 0;
+    private float timeAtkNext = 0;
+
     private Rigidbody rigid;
     private Animator anim;
     private Vector3[] directions = new Vector3[] { Vector3.right, Vector3.up, Vector3.left, Vector3.down };
     private KeyCode[] keys = new KeyCode[] { KeyCode.RightArrow, KeyCode.UpArrow, KeyCode.LeftArrow, KeyCode.DownArrow };
+
     void Awake()
     {
         rigid = GetComponent<Rigidbody>();
     }
     void Update()
     {
+        //Handle Keyboard Input and manage eDrayModes
         dirHeld = -1;
         for (int i = 0; i < 4; i++)
         {
-            if (Input.GetKey(keys[i])) dirHeld = i; 
+            if (Input.GetKey(keys[i])) dirHeld = i;
         }
-        Vector3 vel = Vector3.zero;
-        if (dirHeld > -1) vel = directions[dirHeld];
-        rigid.velocity = vel * speed;
-        if (dirHeld == -1)
-        { 
-            anim.speed = 0;
-        }
-        else
+        if (Input.GetKeyDown(KeyCode.Z) && Time.time >= timeAtkNext)
         {
-            anim.CrossFade("Dray_Walk_" + dirHeld, 0); 
-            anim.speed = 1;
+            mode = eMode.attack;
+            timeAtkDone = Time.time + attackDuration;
+            timeAtkNext = Time.time + attackDelay;
         }
+        if (Time.time >= timeAtkDone)
+        {
+            mode = eMode.idle;
+        }
+        if (mode != eMode.attack)
+        {
+            if (dirHeld == -1)
+            {
+                mode = eMode.idle;
+            }
+            else
+            {
+                facing = dirHeld;
+                mode = eMode.move;
+            }
+        }
+        //Act on the current mode
+        Vector3 vel = Vector3.zero;
+        switch (mode)
+        {
+            case eMode.attack:
+                anim.CrossFade("Dray_Attack_" + facing, 0);
+                anim.speed = 0;
+                break;
+            case eMode.idle:
+                anim.CrossFade("Dray_Walk_" + facing, 0);
+                anim.speed = 0;
+                break;
+            case eMode.move:
+                vel = directions[dirHeld];
+                anim.CrossFade("Dray_Walk_" + facing, 0);
+                anim.speed = 1;
+                break;
+        }
+        rigid.velocity = vel * speed;
     }
 }
