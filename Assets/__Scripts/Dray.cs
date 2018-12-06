e17bb881f9e896ce6672ba1f7d8fabd7dc141b01
lalitakella <31657061+lalitakella@users.noreply.github.com>
2018-12-06 17:01:37 -0500

    IFacingMover

--- Assets/__Scripts/Dray.cs
+++ Assets/__Scripts/Dray.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dray : MonoBehaviour
+public class Dray : MonoBehaviour, IFacingMover
 {
     public enum eMode { idle, move, attack, transition }
 
@@ -21,6 +21,7 @@ public class Dray : MonoBehaviour
 
     private Rigidbody rigid;
     private Animator anim;
+    private InRoom inRm;
     private Vector3[] directions = new Vector3[] { Vector3.right, Vector3.up, Vector3.left, Vector3.down };
     private KeyCode[] keys = new KeyCode[] { KeyCode.RightArrow, KeyCode.UpArrow, KeyCode.LeftArrow, KeyCode.DownArrow };
 
@@ -75,5 +76,39 @@ public class Dray : MonoBehaviour
                 break;
         }
         rigid.velocity = vel * speed;
+    }
+    public int GetFacing()
+    { 
+        return facing;
+    }
+    public bool moving
+    { 
+        get
+        {
+            return (mode == eMode.move);
+        }
+    }
+    public float GetSpeed()
+    { 
+        return speed;
+
+    }
+    public float gridMult
+    {
+        get { return inRm.gridMult; }
+    }
+    public Vector2 roomPos
+    { 
+        get { return inRm.roomPos; }
+        set { inRm.roomPos = value; }
+    }
+    public Vector2 roomNum
+    {
+        get { return inRm.roomNum; }
+        set { inRm.roomNum = value; }
+    }
+    public Vector2 GetRoomPosOnGrid(float mult = -1)
+    {
+        return inRm.GetRoomPosOnGrid(mult);
     }
 }
