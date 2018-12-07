c05bc190d285e1015039fbbf28fd6b63398185f5
lalitakella <31657061+lalitakella@users.noreply.github.com>
2018-12-06 21:23:24 -0500

    Skeletos takes damage

--- Assets/__Scripts/Skeletos.cs
+++ Assets/__Scripts/Skeletos.cs
@@ -20,8 +20,10 @@ public class Skeletos : Enemy, IFacingMover
         inRm = GetComponent<InRoom>();
     }
 
-        void Update()
-    {
+    override protected void Update()
+    { 
+        base.Update();
+        if (knockback) return;
         if (Time.time >= timeNextDecision)
         { 
             DecideDirection();
